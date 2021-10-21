45c6efc12065af25f1e91bc0393233e873b94e3b
millerr2024 <71241942+millerr2024@users.noreply.github.com>
2021-10-21 13:41:59 -0500

    Update convert.py

--- olympics/convert.py
+++ olympics/convert.py
@@ -64,7 +64,7 @@ for row in noc_regions_file_reader:
             notes = row[2]
         list_of_country_rows.append(Row_from_country(NOC, region, notes))
     row_num +=1
-'''
+
 with open('athlete.csv', 'w') as athlete_file:
     writer = csv.writer(athlete_file)
     previously_added_athlete_ids = []
@@ -78,7 +78,7 @@ with open('country.csv', 'w') as country_file:
     for country_row in list_of_country_rows:
         writer.writerow([country_row.NOC, country_row.region, country_row.notes])
    
-'''
+
 with open('athlete_and_country_and_year.csv', 'w') as athlete_and_country_and_year:
     writer = csv.writer(athlete_and_country_and_year)
     #dictionary contains athlete id as key and year as value
@@ -90,16 +90,14 @@ with open('athlete_and_country_and_year.csv', 'w') as athlete_and_country_and_ye
         elif ((athlete_row.year in dictionary.get(athlete_row.id_num)) == False):
             dictionary.get(athlete_row.id_num).append(athlete_row.year)
             writer.writerow([athlete_row.id_num, athlete_row.NOC, athlete_row.year])
-''' 
+
 with open('NOC_and_team.csv', 'w') as NOC_and_team:
     writer = csv.writer(NOC_and_team)
     previously_added_team = []
     for athlete_row in list_of_athlete_rows:
         if (athlete_row.team in previously_added_team) == False:
             writer.writerow([athlete_row.NOC, athlete_row.team])
-            previously_added_team.append(athlete_row.team)
-            
-            
+            previously_added_team.append(athlete_row.team)          
 
 with open('get_city.csv', 'w') as get_city:
     writer = csv.writer(get_city)
@@ -133,4 +131,4 @@ with open('get_event.csv', 'w') as get_event:
     for key in previously_added_information:
         for value in previously_added_information.get(key):
             writer.writerow([key, value])
-'''
+
